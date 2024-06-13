f53f0fd30c54fe106e5e51455968242d5bda7fdc
GabeDiiaas <gabrielgarfieldorg@gmail.com>
2024-06-12 23:55:23 -0300

    adicionando musica e itens

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ document.getElementById('botao').addEventListener('click', function() {
     }
 });
 
+document.getElementById('botao 2').addEventListener('click', function() {
+  var texto = document.getElementById('receita 2');
+  if (texto.style.display === 'none') {
+      texto.style.display = 'block';
+  } else {
+      texto.style.display = 'none';
+  }
+});
+
+document.getElementById('botao 3').addEventListener('click', function() {
+  var texto = document.getElementById('receita 3');
+  if (texto.style.display === 'none') {
+      texto.style.display = 'block';
+  } else {
+      texto.style.display = 'none';
+  }
+});
+
 let slideIndex = 0;
 showSlides();
 
@@ -21,3 +39,19 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 3000);
 }
+
+document.getElementById('playButton').addEventListener('click', function() {
+  var audio = document.getElementById('musica');
+  audio.play();
+});
+
+document.getElementById('playButton 2').addEventListener('click', function() {
+  var audio = document.getElementById('musica 2');
+  audio.play();
+});
+
+
+document.getElementById('playButton 3').addEventListener('click', function() {
+  var audio = document.getElementById('musica 3');
+  audio.play();
+});
